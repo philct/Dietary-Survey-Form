20b82ae140d11cc0d96640817e32723e82c724eb
philct <philct00@gmail.com>
2025-11-26 23:51:18 -0500

    Add Instance variables for CustomJFrame class

--- CustomJFrame.java
+++ CustomJFrame.java
@@ -1,5 +1,50 @@
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JSlider;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
 
-public class CustomJFrame 
+public class CustomJFrame extends JFrame
 {
-
+	// Declaring Instance variables
+	private JLabel headingLabel;
+	private JLabel firstNameLabel;
+	private JLabel lastNameLabel;
+	private JLabel phoneNumberLabel;
+	private JLabel emailLabel;
+	private JLabel dietaryLabel;
+	private JLabel genderLabel;
+	private JLabel waterLabel;
+	private JLabel mealsLabel;
+	private JLabel checkBoxLabel;
+	private JLabel walkLabel;
+	private JLabel weightLabel;
+	private JTextField firstNameTextField;
+	private JTextField lastNameTextField;
+	private JTextField phoneNumberTextField;
+	private JTextField emailTextField;
+	private JRadioButton maleRadioButton;
+	private JRadioButton femaleRadioButton;
+	private JRadioButton preferRadioButton;
+	private ButtonGroup radioButtonGroup;
+	private JSpinner waterIntakeSpinner;
+	private JSlider mealSlider;
+	private JCheckBox wheatCheckBox;
+	private JCheckBox sugarCheckBox;
+	private JCheckBox dairyCheckBox;
+	private JComboBox walkComboBox;
+	private String[] walkOptions;
+	private JFormattedTextField weightFormattedTextField;
+	private JButton clearButton;
+	private JButton submitButton;
+	private FileHandler fileHandler;
+	
+	
+	
 } // End of class
--- Project4.java
+++ Project4.java
@@ -5,6 +5,7 @@ public class Project4
 	public static void main(String[] args) 
 	{
 		
+		
 	} // End of main
 
 } // End of class
