7dc5db6abd04c1e29c501955ab9cc6d06a828d32
philct <philct00@gmail.com>
2025-11-27 00:01:24 -0500

    Create instance variables for FileHandler.java Class

--- FileHandler.java
+++ FileHandler.java
@@ -1,5 +1,12 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class FileHandler 
 {
-
+	// Declaring instance variables
+	private String surveyFile;
+	private FileWriter fileOutput;
+	private PrintWriter printWriter;
+	
+	
 } // End of class
